b45300bc18ea2a1fb9d7ee35f80ccd8adf8298bf
WallyNegima <u.str.gm@gmail.com>
2017-07-03 09:12:57 +0900

    呟きに日数を含むようにした

--- labactivityd.py
+++ labactivityd.py
@@ -106,60 +106,61 @@ def labactivity():
 	cursor = connector.cursor()
 
 	while(1):
-        # データベースの日付一覧を取得
-        cursor.execute('select * from temp_values')
-        records = cursor.fetchall()
+			# データベースの日付一覧を取得
+			cursor.execute('select * from temp_values')
+			records = cursor.fetchall()
 
-        d = datetime.date.today()
-        cursor.execute('select * from temp_values where date = date(now())' )
-        today = cursor.fetchall()
-        print(today)
+			d = datetime.date.today()
+			cursor.execute('select * from temp_values where date = date(now())' )
+			today = cursor.fetchall()
+			print(today)
 
-        if len(today) == 0:
-            # 初めての日付なら…
-            # 今日の日付分を格納してhourに0を格納
-            # データベースを明後日、総プレイ時間を計算
-            cursor.execute('select hour from temp_values')
-            data = cursor.fetchall()
-            # print data
+			if len(today) == 0:
+					# 初めての日付なら…
+					# 今日の日付分を格納してhourに0を格納
+					# データベースを明後日、総プレイ時間を計算
+					cursor.execute('select hour from temp_values')
+					data = cursor.fetchall()
+					# print data
 
-            hours = np.array(data) # arrayに変形
-            hours = hours[:,0] # タプルが入っているので数値だけの１次元arrayに整形
-            hours = hours.astype(np.int64) # 中身をすべて整数型にキャスト
-            total = hours.sum()
-            # print total
+					hours = np.array(data) # arrayに変形
+					hours = hours[:,0] # タプルが入っているので数値だけの１次元arrayに整形
+					hours = hours.astype(np.int64) # 中身をすべて整数型にキャスト
+					total = hours.sum()
+					# print total
 
-            lab_minutes = (total*10)/60
-            lab_hours = lab_minutes/60
-            lab_minutes = lab_minutes - lab_hours*60
-            cursor.execute('insert into temp_values (date, hour) values (date(now()), 0)')
-            tweet('本日までのウォーリーの研究活動時間は約 %d 時間 %d 分 です。' %(lab_hours, lab_minutes))
-            # database commit
-            connector.commit()
-        # 光度取得
-        temp = analog_read()
-        print temp
+					lab_minutes = (total*10)/60
+					lab_days = lab_minutes/(60*24)
+					lab_hours = (lab_minutes - lab_days*(60*24))/60
+					lab_minutes = lab_minutes - lab_days*60*24  - lab_hours*60
+					cursor.execute('insert into temp_values (date, hour) values (date(now()), 0)')
+					tweet('本日までのウォーリーの研究活動時間は約%d 日と %d 時間 %d 分 です。' %(lab_days, lab_hours, lab_minutes))
+					# database commit
+					connector.commit()
+		# 光度取得
+			temp = analog_read()
+			print temp
 
-        if int(temp) < 40:
-            # 指で抑えていないときの数値
-            # ようするに明るいとき
-            # 部屋の普通の明るさ=25
+			if int(temp) < 40:
+					# 指で抑えていないときの数値
+					# ようするに明るいとき
+					# 部屋の普通の明るさ=25
 
-            # 距離取得
-            distance = reading(0)
-            print distance
+					# 距離取得
+					distance = reading(0)
+					print distance
 
-            time.sleep(1)
-            if float(distance) > MIN_RANGE and float(distance) < MAX_RANGE:
-                # hourだけアップデートして1追加する
-                cursor.execute('update temp_values set hour = 1 + %d where date = date(now())' %(today[0][1]))
-                # database commit
-                connector.commit()
-        else:
-            # 指で抑えると暗くなって数値があがる
-            # 要するに暗いとき
-            pass
-        time.sleep(10)
+					time.sleep(1)
+					if float(distance) > MIN_RANGE and float(distance) < MAX_RANGE:
+							# hourだけアップデートして1追加する
+							cursor.execute('update temp_values set hour = 1 + %d where date = date(now())' %(today[0][1]))
+							# database commit
+							connector.commit()
+			else:
+					# 指で抑えると暗くなって数値があがる
+					# 要するに暗いとき
+					pass
+			time.sleep(10)
 
 	# database close
 	cursor.close()
