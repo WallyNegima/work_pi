4e3d953870063299df3847855c4e5adda838516a
WallyNegima <u.str.gm@gmail.com>
2017-07-23 15:43:30 +0900

    python2系で走査可能

--- touchSensor.py
+++ touchSensor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -✳- coding: utf-8 -✳- 
 import os
 import time
 import RPi.GPIO as GPIO
@@ -10,17 +10,13 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 # TRIG = for distance sensor
-TRIG = [17,27,22,11,5,6,13,19]
-ECHO = [18,23,24,25,12,16,20,21]
+TRIG = [19,13,6,5,11,22,27,17]
+ECHO = [21,20,16,12,25,24,23,18]
 
 # 測距センサーで距離測定
 def readDistance(sensor, trig, echo):
-    if sensor == 0:
-        GPIO.setup(trig, GPIO.OUT)
-        GPIO.setup(echo, GPIO.IN)
-        GPIO.output(trig, GPIO.LOW)
-        time.sleep(0.2)
-
+    if sensor >= 0:
+#        print('%d' %sensor)
         GPIO.output(trig, True)
         time.sleep(0.00001)
         GPIO.output(trig, False)
@@ -29,20 +25,29 @@ def readDistance(sensor, trig, echo):
             signaloff = time.time()
 
         while GPIO.input(echo) == 1:
-            signalon = time.time()
+          signalon = time.time()
 
         timepassed = signalon - signaloff
         distance = timepassed * 17000
         return distance
         GPIO.cleanup()
     else:
-        print("Incorrect usonic() function variable")
+        print "Incorrect usonic() function variable"
 
 def main():
-  while ture:
-    distance = readDistance(0, TRIG[0], ECHO[0])
-    print(distance)
-    time.sleep(1000)
+  print "start!"
+  for i in range(8):
+    GPIO.setup(TRIG[i], GPIO.OUT)
+    GPIO.setup(ECHO[i], GPIO.IN)
+    GPIO.output(TRIG[i], GPIO.LOW)
+    time.sleep(0.2)
+
+  while(1):
+    for i in range(8):
+      distance = readDistance(i, TRIG[i], ECHO[i])
+      print(distance)
+      time.sleep(0.01)
+    print "-----------"
 
 if __name__ == "__main__":
   main()
